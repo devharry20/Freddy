d3cbdcafc1163af81a0029bc91344795dd9d614b
Harry <info@devharry.co.uk>
2021-05-15 17:45:31 +0100

    added default bot colour and check emoji

--- freddy/utils/config.py
+++ freddy/utils/config.py
@@ -3,5 +3,7 @@ from typing import NamedTuple
 
 class Config(NamedTuple):
     PREFIX = "f!"
+    DEFAULT_COLOUR = 0xF2C572
 
     SPOTIFY_EMOJI_ID = 842684777004924978
+    CHECK_EMOJI_ID =843165240010080337
